e8bba6f010308cb6474d342292b06405b5f543a3
Manish yepuri <manishchowdary2006@gmail.com>
2025-04-13 21:12:28 +0530

    Delete att directory

--- att/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-# Initialize database
-db = SQLAlchemy()
-
-# Initialize login manager
-login_manager = LoginManager()
-login_manager.login_view = 'login'
